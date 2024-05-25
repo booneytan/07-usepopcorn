ccca0387fec6948ea0f4d846ac26792cad2b7682
booneytan <73635818+booneytan@users.noreply.github.com>
2024-05-25 16:58:42 +0800

    Learn about useEffect

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-//import "./index.css";
-//import App from "./App";
+import "./index.css";
+import App from "./App";
 import StarRating from "./StarRating";
 import { useState } from "react";
 
@@ -19,7 +19,7 @@ function Test() {
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    {/*<App />*/}
+    <App />
     <StarRating maxRating={5} messages={("Terrible", "Bad", "Okay", "Good", "Amazing")} />
 
     <StarRating size={24} color="red" className="testRed" defaultRating={3} />
